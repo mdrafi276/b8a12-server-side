1dc1ffcb8586e5f92d7ce5be519d5a21c83bb8fa
mdrafi276 <mdrafi37473@gmail.com>
2023-11-28 14:27:44 +0600

    jwt token implement and use post mathord on jwt

--- index.js
+++ index.js
@@ -54,24 +54,15 @@ async function run() {
 
 
 
-
-
-    // middlewares
-    const verifyToken = (req, res, next) => {
-      console.log("inside verify token", req.headers.authorization);
-      if (!req.headers.authorization) {
-        return res.status(401).send({ message: "unauthorized access" });
-      }
-      const token = req.headers.authorization;
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-          return res.status(401).send({ message: "unauthorized access" });
-        }
-        req.decoded = decoded;
-        next();
+app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "10d",
       });
-    };
+      res.send(token);
+    });
 
+   
 // user data api 
 
      app.post("/users", async (req, res) => {
