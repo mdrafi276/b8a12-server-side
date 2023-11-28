5e050c2d91d8837097b2d17652eddf7038eee64f
mdrafi276 <mdrafi37473@gmail.com>
2023-11-28 14:28:25 +0600

    token verify

--- index.js
+++ index.js
@@ -62,7 +62,22 @@ app.post("/jwt", async (req, res) => {
       res.send(token);
     });
 
-   
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
+      }
+      const token = req.headers.authorization;
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
 // user data api 
 
      app.post("/users", async (req, res) => {
